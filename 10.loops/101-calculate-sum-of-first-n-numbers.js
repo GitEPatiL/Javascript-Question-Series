f8e6985d7aaa764498cecef508f490117636b254
Aviraj <giteaviraj155@gmail.com>
2026-03-02 18:27:09 +0530

    question solved

--- 10.loops/101-calculate-sum-of-first-n-numbers.js
+++ 10.loops/101-calculate-sum-of-first-n-numbers.js
@@ -1,7 +1,17 @@
 // Question: Calculate sum of first n numbers
 
 // Write your solution below:
-function sumFirstN(n) {
-    // Your code here
+let n = 5;
+let sum = 0;
+
+for(let i=0;i<=5;i++){
+    sum+=i;
+
 }
+console.log(`The Tmotal sum of N numbers is ${sum}`)
+    
+    
+    
+
+
 
