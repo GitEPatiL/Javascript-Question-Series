4732760f2b2d8254dedc7f0a45f68da966445ae8
Aviraj Gite <giteaviraj155@gmail.com>
2026-05-02 20:05:14 +0530

    update 7th question

--- 10.loops/07-count-digits-of-positive-integer.js
+++ 10.loops/07-count-digits-of-positive-integer.js
@@ -1,7 +1,14 @@
 // Q7: Count the digits of a positive integer using loops (no string conversion).
 
+//positive integer = all positive numbers after 0
+
 // Write your solution below:
+
+ 
 function countDigits(n) {
-    // Your code here
+    if(n)
+    
 }
+
+countDigits();
 
